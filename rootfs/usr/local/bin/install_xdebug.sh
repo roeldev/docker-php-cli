4f4165723edd0c4d273548330efe5e7711d5f8ff
roeldev <roeldev@users.noreply.github.com>
2019-07-10 22:55:31 +0200

    feat: Add Xdebug install script

---
+++ rootfs/usr/local/bin/install_xdebug.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+phpVersion=$( php -r "echo phpversion();" | cut -d'.' -f 1-2 )
+
+curl -fLs \
+    -o /tmp/xdebug.tgz \
+    https://xdebug.org/files/xdebug-2.7.2.tgz
+
+apk add \
+    --no-cache \
+    --virtual .phpize-dependencies \
+    php7.1-dev
+
+tar -xzf /tmp/xdebug.tgz -C /tmp/
+cd /tmp/xdebug-2.7.2
+
+phpize && ./configure && make
+
+cp modules/xdebug.so /usr/lib/php/7.1/modules
+touch /etc/php/7.1/conf.d/xdebug.ini
+echo "zend_extension=/usr/lib/php/7.1/modules/xdebug.so" >> /etc/php/7.1/conf.d/xdebug.ini
+
+apk del .phpize-dependencies
+rm -rf /tmp/*
+
+echo
+php -v
